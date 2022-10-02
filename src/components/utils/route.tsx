3cf6f59b93cfa8ef39e1d6f5a06e7738f8fa7c4b
Aleksander Aleksievich <aleksievisa@gmail.com>
2022-10-02 19:04:55 +0300

    init

--- src/components/utils/route.tsx
+++
@@ -1,9 +0,0 @@
-import About from 'page/About';
-import Home from 'page/Home';
-import Welcome from 'page/Welcome';
-
-export const route = [
-  { path: '/', component: Welcome, name: 'Home' },
-  { path: 'about', component: About, name: 'About' },
-  { path: 'home', component: Home, name: 'Home' },
-];
